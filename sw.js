5a1ed0779217d81c711f0f82a74c7f71cb2a424c
chojaeyoun <52194772+chojaeyoun@users.noreply.github.com>
2026-06-16 15:22:46 +0900

    Refresh Sushi Vinegar app icon

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "exam-study-app-v24";
+const CACHE_NAME = "exam-study-app-v25";
 const APP_SHELL = [
   "./index.html",
   "./industrial_safety_study.html",
   "./industrial_safety_past_questions.js",
   "./manifest.webmanifest",
-  "./icons/safety-icon.svg",
+  "./icons/sushi-vinegar-icon.svg",
   "./industrial_safety_questions_template.csv"
 ];
 
